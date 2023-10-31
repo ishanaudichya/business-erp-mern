ad3693da4c07a462a421d313dcc101decd47271f
ishanaudichya <93155612+ishanaudichya@users.noreply.github.com>
2023-10-31 23:53:50 +0530

    init

---
+++ client/src/pages/Homepage.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from "react";
+import DefaultLayout from "./../components/DefaultLayout";
+import axios from "axios";
+import { Row, Col } from "antd";
+import { useDispatch } from "react-redux";
+import ItemList from "../components/ItemList";
+const Homepage = () => {
+  const [itemsData, setItemsData] = useState([]);
+  const [selecedCategory, setSelecedCategory] = useState("drinks");
+  const categories = [
+    {
+      name: "drinks",
+      imageUrl: "https://cdn-icons-png.flaticon.com/512/430/430561.png",
+    },
+    {
+      name: "rice",
+      imageUrl: "https://cdn-icons-png.flaticon.com/512/3174/3174880.png",
+    },
+    {
+      name: "noodles",
+      imageUrl: "https://cdn-icons-png.flaticon.com/512/1471/1471262.png",
+    },
+  ];
+  const dispatch = useDispatch();
+
+  //useEffect
+  useEffect(() => {
+    const getAllItems = async () => {
+      try {
+        dispatch({
+          type: "SHOW_LOADING",
+        });
+        const { data } = await axios.get("/api/items/get-item");
+        setItemsData(data);
+        dispatch({ type: "HIDE_LOADING" });
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getAllItems();
+  }, [dispatch]);
+  return (
+    <DefaultLayout>
+      <div className="d-flex">
+        {categories.map((category) => (
+          <div
+            key={category.name}
+            className={`d-flex category ${
+              selecedCategory === category.name && "category-active"
+            }`}
+            onClick={() => setSelecedCategory(category.name)}
+          >
+            <h4 className="text-uppercase text-white">{category.name}</h4>
+            <img
+              src={category.imageUrl}
+              alt={category.name}
+              height="40"
+              width="60"
+            />
+          </div>
+        ))}
+      </div>
+      <Row>
+        {itemsData
+          .filter((i) => i.category === selecedCategory)
+          .map((item) => (
+            <Col xs={24} lg={6} md={12} sm={6}>
+              <ItemList key={item.id} item={item} />
+            </Col>
+          ))}
+      </Row>
+    </DefaultLayout>
+  );
+};
+
+export default Homepage;
