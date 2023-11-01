876655000013b60483af4c1af188fed406a43845
ishanaudichya <93155612+ishanaudichya@users.noreply.github.com>
2023-11-01 11:37:23 +0530

    vert

---
+++ client/src/App.js
@@ -0,0 +1,72 @@
+import "antd/dist/antd.min.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import CartPage from "./pages/CartPage";
+import Homepage from "./pages/Homepage";
+import ItemPage from "./pages/ItemPage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import BillsPage from "./pages/BillsPage";
+import CutomerPage from "./pages/CutomerPage";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Homepage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/items"
+            element={
+              <ProtectedRoute>
+                <ItemPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/cart"
+            element={
+              <ProtectedRoute>
+                <CartPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/bills"
+            element={
+              <ProtectedRoute>
+                <BillsPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/customers"
+            element={
+              <ProtectedRoute>
+                <CutomerPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
+
+export function ProtectedRoute({ children }) {
+  if (localStorage.getItem("auth")) {
+    return children;
+  } else {
+    return <Navigate to="/login" />;
+  }
+}
