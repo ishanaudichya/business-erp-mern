6537915e90a2e0bbcf54e7ba8ca0f6836edcb60a
ishanaudichya <93155612+ishanaudichya@users.noreply.github.com>
2023-11-01 11:41:56 +0530

    package and models

---
+++ server/models/billsModel.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const billSchema = mongoose.Schema(
+  {
+    customerName: {
+      type: String,
+      required: true,
+    },
+    customerNumber: {
+      type: Number,
+      required: true,
+    },
+    totalAmount: {
+      type: Number,
+      required: true,
+    },
+    subTotal: {
+      type: Number,
+      required: true,
+    },
+    tax: {
+      type: Number,
+      required: true,
+    },
+    paymentMode: {
+      type: String,
+      required: true,
+    },
+    cartItems: {
+      type: Array,
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now(),
+    },
+  },
+  { timestamp: true }
+);
+
+const Bills = mongoose.model("bills", billSchema);
+
+module.exports = Bills;
